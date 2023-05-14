1f8f99e164c820c49b8d4897c737801fd662a188
claro0 <rosadeclaro@163.com>
2023-05-14 16:13:09 +0800

    https://github.com/gojuyaneko/charlieComUI.git

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,63 @@
 <template>
   <div id="app">
-    <charlie-nav class="app-nav"></charlie-nav>
-   <router-view></router-view>
+    <charlieIndex></charlieIndex>
   </div>
 </template>
 
 <script>
-import charlieNav from '@/components/charlieNav'
+import charlieIndex from "./pages/charlieIndex.vue";
+
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    charlieNav
-  }
-}
+    charlieIndex,
+  },
+};
 </script>
 
 <style>
-*{
+* {
   margin: 0;
-  padding: 0; 
+  padding: 0;
 }
-html,body{
+html,
+body {
   width: 100%;
   height: 100%;
   -webkit-user-drag: none;
 }
-@media screen and (min-width:480px) {
-  html,body {
-    font-size:16px ;
+@media screen and (min-width: 480px) {
+  html,
+  body {
+    font-size: 16px;
   }
 }
-@media screen and (min-width:768px) {
-  html,body {
-    font-size:18px ;
+@media screen and (min-width: 768px) {
+  html,
+  body {
+    font-size: 18px;
   }
 }
-@media screen and (min-width:992px) {
-  html,body {
-    font-size:20px ;
+@media screen and (min-width: 992px) {
+  html,
+  body {
+    font-size: 20px;
   }
 }
-@media screen and (min-width:1200px) {
-  html,body {
-    font-size:22px ;
+@media screen and (min-width: 1200px) {
+  html,
+  body {
+    font-size: 22px;
   }
 }
-li{
+li {
   list-style: none;
 }
 #app {
   width: 100%;
   height: 100%;
 }
-.app-nav{
+.app-nav {
   position: sticky;
   top: 0;
   z-index: 2;
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'//vuex
-import router from './router'//router
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store"; //vuex
+import router from "./router"; //router
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
-Vue.config.productionTip = false
+import navBar from "./components/navBar.vue";
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+Vue.component("myNavbar", navBar);
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
   store,
   router,
-}).$mount('#app')
+}).$mount("#app");
