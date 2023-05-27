abedf0cc3309c59a269607905e32a4f6d337fed8
Chen74 <54929297+Chen74@users.noreply.github.com>
2023-05-26 23:31:12 -0600

    update pages

--- src/pages/subitems/sweet/dreamWeaving/DreamWeaving.vue
+++ src/pages/subitems/sweet/dreamWeaving/DreamWeaving.vue
@@ -2,59 +2,108 @@
   <div class="dream_weaving">
     <div class="titles">
       <ul class="btns">
-          <li class="angel-btn" @click="angelVisible=true"><img src="./image/btn.png" height="37.5" width="110"></li>
-          <li class="evil-btn" @click="evilVisible=true"><img src="./image/btn.png" height="37.5" width="110"></li>
+          <li class="angel-btn" @click="meetVisible=true"><img src="./image/btn.png" height="37.5" width="110"></li>
+          <li class="evil-btn" @click="skyVisible=true"><img src="./image/btn.png" height="37.5" width="110"></li>
       </ul>
     </div>
 
     <div class="angel-dia">
-      <el-dialog class="meet" :visible.sync="angelVisible" :show-close="false" v-if="angelVisible">
+      <el-dialog class="meet" :visible.sync="meetVisible" :show-close="false" v-if="meetVisible" :close-on-click-modal ="false">
         <h4>{{cardText}}</h4>
         <li class="slide"><img src="./image/滑轨.png" height="460" width="30"></li>
-        <li class="x" @click="angelVisible=false"><img src="./image/x.png" height="30" width="30"></li>
-        <li class="next-btn" @click="tripVisible=true;angelVisible=false" ><img src="./image/下一幕.png" height="37.5" width="110"></li>
+        <li class="x" @click="meetVisible=false"><img src="./image/x.png" height="30" width="30"></li>
+        <ul class="elements">
+            <li class="co">第</li>
+            <li class="ch">节</li>
+        </ul>
+        <div class="diapage">
+          <dw-pagi @changeCurPa="getDiaContent"></dw-pagi>
+        </div>
+        <li class="next-btn" @click="tripVisible=true;meetVisible=false" ><img src="./image/下一幕.png" height="37.5" width="110"></li>
+
       </el-dialog>
 
-      <el-dialog class="trip" :visible.sync="tripVisible" :show-close="false" v-if="tripVisible">
+      <el-dialog class="trip" :visible.sync="tripVisible" :show-close="false" v-if="tripVisible" :close-on-click-modal ="false">
         <h4>{{cardText1}}</h4>
         <li class="slide"><img src="./image/滑轨.png" height="460" width="30"></li>
         <li class="x" @click="tripVisible=false"><img src="./image/x.png" height="30" width="30"></li>
-        <li class="next-btn" @click="angelVisible=true;tripVisible=false"><img src="./image/下一幕.png" height="37.5" width="110"></li>
+        <ul class="elements">
+            <li class="co">第</li>
+            <li class="ch">节</li>
+        </ul>
+        <div class="diapage">
+          <dw-pagi @changeCurPa="getDiaContent"></dw-pagi>
+        </div>
+        <li class="next-btn" @click="meetVisible=true;tripVisible=false"><img src="./image/下一幕.png" height="37.5" width="110"></li>
       </el-dialog>
     </div>
 
-    <el-dialog class="evil-dia" :visible.sync="evilVisible" :show-close="false" >
-      <h4>{{cardText}}</h4>
-      <li class="slide"><img src="./image/滑轨.png" height="460" width="30"></li>
-      <li class="x" @click="evilVisible=false"><img src="./image/x.png" height="30" width="30"></li>
-      <li class="next-btn"><img src="./image/下一幕.png" height="37.5" width="110"></li>
-    </el-dialog>
+    <div class="evil-dia">
+      <el-dialog class="sky" :visible.sync="skyVisible" :show-close="false" v-if="skyVisible" :close-on-click-modal ="false">
+        <h4>{{cardText}}</h4>
+        <li class="slide"><img src="./image/滑轨.png" height="460" width="30"></li>
+        <li class="x" @click="skyVisible=false"><img src="./image/x.png" height="30" width="30"></li>
+        <ul class="elements">
+            <li class="co">第</li>
+            <li class="ch">节</li>
+        </ul>
+        <div class="diapage">
+          <dw-pagi @changeCurPa="getDiaContent"></dw-pagi>
+        </div>
+        <li class="next-btn" @click="moonVisible=true;skyVisible=false"><img src="./image/下一幕.png" height="37.5" width="110"></li>
+      </el-dialog>
+
+      <el-dialog class="moon" :visible.sync="moonVisible" :show-close="false" v-if="moonVisible" :close-on-click-modal ="false">
+        <h4>{{cardText1}}</h4>
+        <li class="slide"><img src="./image/滑轨.png" height="460" width="30"></li>
+        <li class="x" @click="moonVisible=false"><img src="./image/x.png" height="30" width="30"></li>
+        <ul class="elements">
+            <li class="co">第</li>
+            <li class="ch">节</li>
+        </ul>
+        <div class="diapage">
+          <dw-pagi @changeCurPa="getDiaContent"></dw-pagi>
+        </div>
+        <li class="next-btn" @click="skyVisible=true;moonVisible=false"><img src="./image/下一幕.png" height="37.5" width="110"></li>
+      </el-dialog>
+    </div>
+
 
   </div>
 
 </template>
 
 <script>
+import dwPagi from '../dreamWeaving/dwPagi'
 export default {
+  components: {dwPagi},
   data(){
     return {
-      angelVisible:false,
-      evilVisible:false,
+      meetVisible:false,
+      skyVisible:false,
       tripVisible:false,
+      moonVisible:false,
       cardText:'刚走进餐馆坐下',
       cardText1:'完美啊你的名字是charlie'
     }
 
-  }
+  },
+    mounted() {},
+    methods() {
+
+    }
 }
 </script>
 
 <style scoped lang="scss">
 .dream_weaving {
+  text-align: center;
+  position: absolute;
   width: 100%;
   height: 100%;
   background: url('./image/背景.png') no-repeat;
   background-size: 100% 100%;
+  
 }
 
 .titles {
@@ -95,7 +144,7 @@ export default {
   transform: scale(1.05);
 }
 
-.meet, .trip {
+.meet, .trip, .sky, .moon {
   background:url("./image/卡牌.png") no-repeat;
   background-size: 79% 70%;
   background-position: center;
@@ -133,6 +182,25 @@ h4 {
 
 .next-btn:hover {
   transform: scale(1.05);
+}
+
+.diapage {
+  top:560px;
+  left:450px;
+  display: inline-block;
+  position: absolute;
+
+}
+
+.elements {
+  display: flex;
+  position: relative;
+  top: 490px;
+  left:495px;
+}
+
+.co {
+  margin-right: 185px;
 }
 
 </style>
---
+++ src/pages/subitems/sweet/dreamWeaving/dwPagi.vue
@@ -0,0 +1,77 @@
+<template>
+<div>
+  <el-pagination
+    :hide-on-single-page="hide"
+    class="dw-pagi"
+    layout="prev, pager, next"
+    :page-count=4
+    @current-change="curChange"
+    prev-text="<"
+    next-text=">">
+  </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  mounted () {},
+  methods: {
+    curChange (currentPage) {
+      this.$emit('changeCurPa',currentPage)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.dw-pagi {
+  width: 750px;
+  height: 40px;
+  overflow: hidden;
+}
+::v-deep {
+  .el-pager li {
+    font-size: 18px;
+    background: transparent;
+    font-weight: 400;
+    min-width: 40px;
+    color: grey;
+  }
+  .el-pager li:hover {
+    color:#7C69A1;
+    transform: scale(1.5);
+  }
+  .el-pager li.active {
+    color:#7C69A1;
+    transform: scale(1.5);
+  }
+
+  .el-pagination button:hover {
+    color:#7C69A1;
+    transform: scale(1.5);
+  }
+  .el-pagination .btn-next, .el-pagination .btn-prev {
+    background: transparent;
+    color:#7C69A1;
+    display: inline-block;
+  }
+  .el-pagination span:not([class*=suffix])  {
+    font-size: 20px;
+  }
+
+  .el-pagination .btn-next {
+    padding-left: 40px;;
+  }
+
+  .el-pagination .btn-prev {
+    padding-right: 40px;
+  }
+
+}
+</style>
