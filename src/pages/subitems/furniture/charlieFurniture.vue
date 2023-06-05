413237161286b5caffcc146339bc89d9bd7945d0
sibyloooooo <zisangxun@126.com>
2023-06-05 23:56:35 +0800

    【add】新增家具主页面

--- src/pages/subitems/furniture/charlieFurniture.vue
+++ src/pages/subitems/furniture/charlieFurniture.vue
@@ -1,23 +1,111 @@
 <template>
-    <div>
-        123
+    <div class="charlie-furniture">
+        <div class="furniture-nav">
+            <span>工作室</span>
+            <span>></span>
+            <span>家具</span>
+        </div>
+        <div class="furniture-title bkg"></div>
+        <div class="furnitre-body bkg">
+            <div class="record bkg" @click="showDeatil('record')"></div>
+            <div 
+                class="btn bkg" 
+                v-for="item in btnList" 
+                :key="item.key"
+                :style="item.style"
+                @click="showDeatil(item.key)"
+            >{{item.name}}</div>
+        </div>
     </div>
 </template>
 
 <script>
-export default{
-    components: {},
+import btnList from './components/btnList'
+export default {
+    mixins: [btnList],
     data() {
-        return{
-
-        }
-    },
-    mounted() {
-
+       return {
+       } 
     },
     methods: {
-
+        showDeatil(e) {
+            console.log('event type:',e);
+        }
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.charlie-furniture {
+    width: 100%;
+    height: 100%;
+    background-size: 100% 100%;
+    background-image: url('./image/furniture-bkg.png');
+    .furniture-nav {
+        top: 200px;
+        left: 150px;
+        font-size: 18px;
+        font-size: 18px;
+        position: absolute;
+        font-family: nansongshuju;
+        color: rgb(206, 172, 99);
+        span {
+            margin-left: 10px;
+            &:first-child {
+                cursor: pointer;
+            }
+        }
+    }
+    .furniture-title {
+        left: 150px;
+        width: 430px;
+        height: 79px;
+        bottom: 350px;
+        position: absolute;
+        background-image: url('./image/title.png');
+        &::after {
+            content: "";
+            top: 50px;
+            width: 517px;
+            height: 53px;
+            position: absolute;
+            background: url('./image/dark-title.png') no-repeat center/100%;
+            background-size: 100% 100%;
+        }
+    }
+    .furnitre-body {
+        bottom: 0;
+        right: 100px;
+        width: 810px;
+        height: 770px;
+        position: absolute;
+        background-image: url('./image/furnitures.png');
+        .record {
+            top: 280px;
+            left: 145px;
+            width: 38px;
+            height: 153px;
+            cursor: pointer;
+            position: absolute;
+            background-image: url('./image/record.png');
+        }
+        .btn {
+            width: 120px;
+            height: 38px;
+            font-size: 20px;
+            cursor: pointer;
+            font-weight: 500;
+            line-height: 38px;
+            text-align: center;
+            position: absolute;
+            font-family: nansongshuju;
+            color: rgb(206, 172, 99);
+            background-image: url('./image/btn-bkg.png');
+        }
+    }
+}
+.bkg {
+    background-size: 100% 100%;
+}
+</style>
 
---
+++ src/pages/subitems/furniture/components/btnList.js
@@ -0,0 +1,58 @@
+export default {
+  data() {
+    return {
+      btnList: [
+        {
+            name: '画板',
+            key: 'drawingBoard',
+            style: 'top: 40px; left: 130px'
+        },
+        {
+            name: '木架',
+            key: 'woodenFrame',
+            style: 'top: 130px; left: 116px'
+        },
+        {
+            name: '音乐',
+            key: 'music',
+            style: 'top: 181px; left: 40px'
+        },
+        {
+            name: '小桌',
+            key: 'table',
+            style: 'left: 128px; bottom: 195px'
+        },
+        {
+            name: '装饰',
+            key: 'decoration ',
+            style: 'left: -28px; bottom: 185px'
+        },
+        {
+            name: '地毯',
+            key: 'carpet',
+            style: 'bottom: 50px; left: -80px'
+        },
+        {
+            name: '画桌',
+            key: 'drawTable',
+            style: 'left: 280px; bottom: 115px'
+        },
+        {
+            name: '画材',
+            key: 'artSupplies',
+            style: 'left: 295px; bottom: 305px'
+        },
+        {
+            name: '椅子',
+            key: 'chair',
+            style: 'bottom: 235px; right: 105px'
+        },
+        {
+            name: '抱枕',
+            key: 'dakimakura',
+            style: 'bottom: 130px; right: 20px'
+        },
+      ]
+    }
+  }
+}
