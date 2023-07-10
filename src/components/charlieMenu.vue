a98d336b1c03a8032f6bb899cb1fae0c54c84526
Chen74 <54929297+Chen74@users.noreply.github.com>
2023-07-10 15:57:32 -0600

    Update charlieMenu.vue

--- src/components/charlieMenu.vue
+++ src/components/charlieMenu.vue
@@ -48,15 +48,15 @@ export default {
           childItems: [
             {
               name: '第一章/ "援塞"之行',
-              nextUrl: "/rescueTrip",
+              nextUrl: "/encounter",
             },
             {
               name: "第二章/ 无名侵蚀",
-              nextUrl: "/unknownErosion",
+              nextUrl: "/encounter",
             },
             {
               name: "第三章/ 真相大白",
-              nextUrl: "/truthCameout",
+              nextUrl: "/encounter",
             },
           ],
         },
