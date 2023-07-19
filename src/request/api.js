57bb062a322fcc48a313a5f3dc4d2d730dd10610
siven <1273318525shi@gmail.com>
2023-07-19 23:42:44 +0800

    视频通话接入完成

--- src/request/api.js
+++ src/request/api.js
@@ -1,10 +1,11 @@
 import { get } from "./http";
 //get和post请求发送例子，第一个参数为url，第二个参数为url所带参数
-const baseUrl = "http://47.243.195.59:8080/";
-export const getTOD = (p) => get(baseUrl + "truthordare", p); // 真心话大冒险接口
-export const getDNMenu = (p) => get(baseUrl + "dayandnight", p); // 主线菜单接口
-export const getWmAll = (p) => get(baseUrl + "moments", p); // 朋友圈总览接口
-export const getVP = (p) => get(baseUrl + "volumePage", p);
+const baseUrl= 'http://47.243.195.59:8080/'
+export const getTOD = p=>get(baseUrl + 'truthordare', p) // 真心话大冒险接口
+export const getDNMenu = p => get(baseUrl + 'dayandnight', p) // 主线菜单接口
+export const getWmAll = p => get(baseUrl + 'moments', p) // 朋友圈总览接口
+export const getVP = p=>get(baseUrl + 'volumePage', p)
+export const getChat = p => get(baseUrl + 'chat', p) // 聊天记录总览接口
 export const getRP = (p) => get(baseUrl + "rewinds", p);
 export const getTP = (p) => get(baseUrl + "tracks", p);
 export const getPP = (p) => get(baseUrl + "profiledetail", p);
